573f996889f5f606445fe98e7906dde664e62353
sjaa47 <sj3216698@gmail.com>
2026-03-04 22:49:36 +0500

    Create tailwind.config.js with black and red theme configuration

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/**
+ * @type {import('tailwindcss').Config}
+ */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        black: '#000000',
+        red: '#FF0000',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
